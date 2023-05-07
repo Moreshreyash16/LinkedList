d7211420cd0783a5fd24bc30b465767d84558de4
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-05-07 20:23:07 +0530

    Add files via upload

--- Liinklist.java
+++ Liinklist.java
@@ -10,10 +10,17 @@ public class Liinklist {
         list.add(70);
         list.add(30);
         list.add(56);
-
+//        uc 2
         list.addFirst(70);
         list.add(30);
         list.add(56);
+
+//        uc 3
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+
+
     }
 
 }
