cf29b3fc19d2cc484f146c7e110179d38a997e9c
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-05-07 20:38:17 +0530

    Update Liinklist.java

--- Liinklist.java
+++ Liinklist.java
@@ -7,10 +7,6 @@ public class Liinklist {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList<Integer>();
-        list.add(70);
-        list.add(30);
-        list.add(56);
-
         list.addFirst(70);
         list.add(30);
         list.add(56);
