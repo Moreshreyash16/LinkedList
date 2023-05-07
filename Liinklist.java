4a67a9545b0940306defd5f51ab7798588199aca
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-05-07 20:19:25 +0530

    Add files via upload

---
+++ Liinklist.java
@@ -0,0 +1,16 @@
+package Bridgelabz.Linklist;
+
+
+import java.util.LinkedList;
+
+public class Liinklist {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList<Integer>();
+        list.add(70);
+        list.add(30);
+        list.add(56);
+
+    }
+
+}
