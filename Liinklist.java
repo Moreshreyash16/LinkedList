c3eaf27c619d31b2ed9a03c1d175a083964418d9
Moreshreyash16 <129577115+Moreshreyash16@users.noreply.github.com>
2023-05-07 21:19:15 +0530

    Add files via upload

--- Liinklist.java
+++ Liinklist.java
@@ -1,6 +1,9 @@
 package Bridgelabz.Linklist;
 
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Liinklist {
@@ -10,7 +13,78 @@ public class Liinklist {
         list.add(70);
         list.add(30);
         list.add(56);
+//        uc 2
+        list.addFirst(70);
+        list.add(30);
+        list.add(56);
 
+//        uc 3
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+
+//        uc4
+        list.addFirst(56);
+        list.add(30);
+        list.add(1,70);
+        Iterator i = list.iterator();
+        while(i.hasNext()){
+            System.out.println(i.next());
+        }
+//        uc5
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+        list.removeFirst();
+        while(i.hasNext()){
+            System.out.println(i.next());
+        }
+//        uc6
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+        list.removeLast();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+//        uc7
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+        while (i.hasNext()) {
+            if(list.contains(30)){
+                System.out.println(i.next());
+                break;
+            }
+        }
+//      uc8
+        int indx=1;
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+        indx=list.indexOf(30);
+        list.add(indx,40);
+
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+ //     uc 9
+        list.addFirst(56);
+        list.add(30);
+        list.add(70);
+        indx=list.indexOf(30);
+        list.add(indx,40);
+        indx=list.indexOf(40);
+        list.remove(indx);
+        System.out.println("Linkedlist size is " + list.size());
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+//      uc10
+        Collections.sort(list);
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
     }
 
 }
